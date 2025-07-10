49859b1627fed048b6d77adb90de51b9e928aa57
Baxtiyor0101 <oqonboyevb@gmail.com>
2025-07-10 22:47:34 +0500

    online

---
+++ L1/app.js
@@ -0,0 +1,161 @@
+// Data Type
+
+// primitive
+// string ""
+// number 1,2.. 34.23
+// bigint  14234342n
+// symbol new Symbol()
+// undefined
+// null
+// let res = null;
+// boolean true false
+
+// nonprimitive
+// object
+// array []
+// object {}
+// functions
+
+// variables
+// let, var, const
+// let ageOf = 25;
+// console.log(ageOf);
+// ageOf = 22;
+// console.log(ageOf);
+
+// const num1 = 234;
+// num1 = 32;
+
+// var age1 = 8;
+// var age1 = 9;
+// console.log(age1);
+
+// let res = {};
+// let res = 32;
+
+// //////////
+// ////////// Arifmetik Operators => +, -, /, *,**,%,
+// console.log(2**5);
+// console.log(12 % 7);
+
+// Unary X++ X-- ++X --X
+// let res = 12;
+// res++;
+// res++;
+// let x = ++res + 9;
+// console.log(x);
+
+// ///////////////
+// += -= *= /=
+// let res = 20;
+// res = res + 2; // res += 2
+// res = res - 3; // res -= 3
+``
+// conditional if , if else, (else if)
+
+// let res = 90;
+// if (res == 8) {
+//   console.log("tog'ri");
+// //   .....
+// }
+
+///////////////////////
+// let res = 93;
+// if (res % 2 == 0) {
+//   console.log("bu juft");
+// } else {
+//   console.log("bu toq");
+// }
+
+// ////////////////
+// let res = 9;
+// if (res == 7) {
+//   console.log("yetti");
+// } else if (res == 0) {
+//   console.log("nol");
+// } else if (res == 9) {
+//   console.log("toqqiz");
+// } else {
+//   console.log("biz kutgan son yoq");
+// }
+
+// //////////////// typeof
+// console.log(typeof 2 === "number");
+// console.log(typeof true);
+
+//////////////// = == ===
+// === strict equality
+// == loose equality
+// console.log(2 === 22);
+
+// /////////////////////
+// comparision operatiors (taqqoslash) >, <, >=, <=, == === != !==,
+// console.log(33 > 3);
+// console.log(2 < 3)
+// console.log(2 <= "2");
+// console.log(2 !== "2");
+// console.log();
+
+// //////////////////
+// //////////////////
+// //////////////////
+// //////////////////
+// homework
+
+// let num = prompt("sonni kiriting");
+// console.log(
+//   num + " X 1 =" + num * 1,
+//   num + " X 2 =" + num * 2,
+//   num + " X 3 =" + num * 3,
+//   num + " X 4 =" + num * 4,
+//   num + " X 5 =" + num * 5,
+//   num + " X 6 =" + num * 6,
+//   num + " X 7 =" + num * 7,
+//   num + " X 8 =" + num * 8,
+//   num + " X 9 =" + num * 9
+// );
+
+// let num = 89;
+// let str = "12";
+// console.log(num + str);
+// console.log("hello");
+// alert("hello here we go !!!"); // info
+// console.log("this is console");
+
+// let res = confirm("are you sure");
+// console.log(res);
+
+// 5
+// let x = 3;
+// let y = 5;
+// let z = 8;
+// x = x + y;
+// y = x - y;
+// x = x - y;
+// // console.log(x);
+// // console.log(y);
+// y = y + z;
+// z = y - z;
+// y = y - z;
+// // console.log(y);
+// // console.log(z);
+
+// // f4
+// let num1 = prompt("birinchi son");
+// let num2 = prompt("ikkinchi son");
+// let num3 = prompt("uchunchi son");
+// let count = 0;
+// // console.log(num1, num2, num3);
+// if (num1 > 0) {
+//   count++;
+// }
+
+// if (num2 > 0) {
+//   count++;
+// }
+
+// if (num3 > 0) {
+//   count++;
+// }
+
+// console.log(count);
---
+++ L2/app.js
@@ -0,0 +1,72 @@
+// type conversions in all cases
+// Truthy and folsy values
+// math methods
+// ternary operator ? :
+
+// type conversions in all cases (bir tipdan ikkinchi tipga otish)
+// number => Number()
+// console.log(Number(true));
+// console.log(Number(false));
+// console.log(Number(""));
+// console.log(Number("23"));
+// console.log(Number(undefined)); // NaN => not a number
+// console.log(Number(null));
+// console.log(Number({}));
+// console.log(Number([23]));
+// console.log(Number(12686768797697987698769n));
+// let res = Symbol("test");
+// console.log(res);
+// console.log(Number("erwr"));
+
+// string => toString()
+// let res = "{}";
+// console.log(typeof String(res));
+// console.log(typeof res.toString());
+
+//  boolean => Boolean()
+
+// falsy values
+// console.log(Boolean(0));
+// console.log(Boolean(""));
+// console.log(Boolean(false));
+// console.log(Boolean(null));
+// console.log(Boolean(undefined));
+// console.log(Boolean(0n));
+// console.log(Boolean(NaN));
+// if ("2" === 2) {
+// }
+
+// Math Methods
+// console.log(Math.pow(4, 1 / 2));
+// console.log(Math.sqrt(4));
+// console.log(Math.cbrt(8));
+// console.log(Math.abs(12));
+// console.log(Math.cos(1));
+
+// let num = 12.904;
+// console.log(Math.floor(num));
+// console.log(Math.ceil(23.4));
+// console.log(Math.round(12.3));
+// console.log(Math.max(2, 3, 6, 4, 2, 4, 6, 8, 4));
+// console.log(Math.min(3, 4, 5, 7, 4, 2));
+// console.log(Math.floor(Math.random()*100)); // 0 - 1  orasidagi ixtiyoriy son
+
+// 50 100
+
+// //////////////////////
+// //////////////////////
+// //////////////////////
+// ternary operator ? : => if else
+
+// let num = 13;
+// let res = num % 2 == 0 ? "juft" : "toq";
+// console.log(res);
+// let res =
+//   num % 3 == 2
+//     ? "test"
+//     : num % 3 == 0
+//     ? "best"
+//     : num % 5 == 0
+//     ? "bu 5ga bolinadi"
+//     : null;
+// console.log(res);
